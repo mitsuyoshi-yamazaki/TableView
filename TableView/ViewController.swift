0d5aad7a9afb42822b30da6ebe545308a39180c4
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2020-05-16 02:56:23 +0900

    Setup project

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
